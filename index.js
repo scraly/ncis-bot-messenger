d0ea186ea2d59a383146f5261d68902c31f121e9
scraly <scraly@gmail.com>
2017-01-03 10:20:08 +0100

    improve

--- index.js
+++ index.js
@@ -30,3 +30,105 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'));
 });
+
+//Facebook page access token
+var token = "EAAFAFUFdjecBAOkw3fNa8Fgc9YVO6X3RZAOLr1TEsrIUWs153Rna78o3Epy7zktZAtd2aBu2ZCmmZChZAu9AZAW5No22biwiKAZB4jMxNcBs8bGtsCk9Ov5UATohoNfgDK2tvgJ1PH1Q9lya4qIbW5Y8wnSouqOe1QGkiYydBAknQZDZD";
+
+//Get messages
+app.post('/webhook/', function (req, res) {
+var messaging_events = req.body.entry[0].messaging
+for (var i = 0; i < messaging_events.length; i++) {
+	var event = req.body.entry[0].messaging[i];
+	var sender = event.sender.id;
+	if (event.message && event.message.text) {
+		var text = event.message.text;
+            if (text === 'Cards') {
+                sendCardMessage(sender);
+	            continue;
+	        }
+			sendTextMessage(sender, "Message reçu : " + text.substring(0, 200));
+		}		
+		//Reagir au click sur un bouton d'une card
+		if (event.postback) {
+			var text = JSON.stringify(event.postback);
+			sendTextMessage(sender, "Postback reçu : "+text.substring(0, 200), token);
+			continue;
+      }
+		
+	}
+	
+		
+	res.sendStatus(200);
+});
+
+//TODO lorsque l'utilisateur saisit "ncis" ou "gibbs rule" alors envoyer en random une des regles de gibbs via fb messenger
+
+//Send messages
+function sendTextMessage(sender, text) {
+    var messageText = { text:text };
+    request({
+        url: 'https://graph.facebook.com/v2.7/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageText,
+	        }
+	    }, function(error, response, body) {
+	        if (error) {
+	            console.log('Une erreur est survenue : ', error);
+	        } else if (response.body.error) {
+	            console.log('Erreur: ', response.body.error);
+	        }
+	    });
+}
+
+//Send card messages
+function sendCardMessage(sender) {
+    var messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Premiere carte",
+                    "subtitle": "Element 1 de la liste",
+	                    "image_url": "http://blog.casino770.com/media/blogs/fr4/quick-uploads/p4834/ncis.jpg?mtime=1468820328",
+	                    "buttons": [{
+	                        "type": "web_url",
+	                        "url": "https://www.messenger.com",
+	                        "title": "Visiter le site"
+	                    }, {
+	                        "type": "postback",
+	                        "title": "Acheter",
+	                        "payload": "Clic sur la premiere carte",
+	                    }],
+	                }, {
+	                    "title": "Deuxieme carte",
+	                    "subtitle": "Element numero 2 de la liste",
+	                    "image_url": "http://papystreaming.com/fr/files/2012/07/NCIS.jpg",
+	                    "buttons": [{
+	                        "type": "postback",
+	                        "title": "Acheter",
+	                        "payload": "Clic sur la deuxieme carte",
+	                    }],
+	                }]
+	            }
+	        }
+	    };
+	    request({
+	        url: 'https://graph.facebook.com/v2.7/me/messages',
+	        qs: {access_token:token},
+	        method: 'POST',
+	        json: {
+	            recipient: {id:sender},
+	            message: messageData,
+	        }
+	    }, function(error, response, body) {
+	        if (error) {
+	            console.log('Erreur pendant l\'envoi du message ', error)
+	        } else if (response.body.error) {
+	            console.log('Erreur: ', response.body.error)
+	        }
+	    });
+}
