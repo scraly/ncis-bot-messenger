abad02a41a1d366317a41d7a14f81b81eb70879a
scraly <scraly@gmail.com>
2017-01-03 09:52:47 +0100

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# ncis-bot-messenger
+
+##pre-requis :
+
+$ sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+$ curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+$ sudo apt-get update
+$ sudo apt-get install heroku
+
+Verify your installation
+vachea@PC-03:~$ heroku --version
+heroku-cli/5.6.10-249d061 (linux-386) go1.7.4
+
+vachea@PC-03:~$ node --version
+v5.0.0
+
+vachea@PC-03:~$ sudo npm install npm -g
+
+##
+
+
+##
+
+
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, je suis un chat bot');
+});
+
+// Webhook route - Facebook authentication and verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'your_password') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Erreur, mauvais token');
+});
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'));
+});
